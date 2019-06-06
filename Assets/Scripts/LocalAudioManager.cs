7629380e99ec323db32e04fa984c9a63e7c25180
joekelly <joekelly@microsoft.com>
2019-06-05 17:51:04 -0700

    Cleared out AudioSources and events, hooked up AudioManager

---
+++ Assets/Scripts/LocalAudioManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Audio;
+
+public class LocalAudioManager : MonoBehaviour
+{
+    public void PlayAudioEvent(AudioEvent eventToPlay)
+    {
+        AudioManager.PlayEvent(eventToPlay, this.gameObject);
+    }
+}
